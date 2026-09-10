c2f9a0c27a3b4a2b0398b20620cc31a22883c873
Guidoka7 <gquavx@gmail.com>
2026-09-10 16:24:11 -0300

    feat: adicionar compatibilidade de links para o painel React

---
+++ src/shims/next-link.tsx
@@ -0,0 +1,22 @@
+import type { AnchorHTMLAttributes, ReactNode } from "react";
+
+type Props = AnchorHTMLAttributes<HTMLAnchorElement> & { href: string; children?: ReactNode; prefetch?: boolean };
+
+export default function Link({ href, children, onClick, ...props }: Props) {
+  return (
+    <a
+      href={href}
+      {...props}
+      onClick={(event) => {
+        if (!event.defaultPrevented && event.button === 0 && !event.metaKey && !event.ctrlKey && !event.shiftKey && !event.altKey) {
+          event.preventDefault();
+          window.history.pushState({}, "", href);
+          window.dispatchEvent(new Event("app:navigate"));
+        }
+        onClick?.(event);
+      }}
+    >
+      {children}
+    </a>
+  );
+}
