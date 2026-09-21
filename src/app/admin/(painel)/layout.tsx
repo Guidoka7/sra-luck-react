e5673c19edb1aff36a501fcbea3dff6a56949fd4
Guidoka7 <gquavx@gmail.com>
2026-09-21 18:55:02 -0300

    security: remove production time simulation control

--- src/app/admin/(painel)/layout.tsx
+++ src/app/admin/(painel)/layout.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import { AdminZipShell } from "@/components/admin-zip/AdminZipShell";
-import { CalendarioTesteTempo } from "@/components/admin/CalendarioTesteTempo";
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
     <AdminZipShell>
-      <CalendarioTesteTempo />
       {children}
     </AdminZipShell>
   );
